f5244d5cd78324a06a77aec7944aabd60929ac9f
Alejandro Romero <alejo8591@gmail.com>
2012-06-05 18:52:11 -0500

    update file finalquiz1

--- finalquiz1.py
+++ finalquiz1.py
@@ -1,3 +1,12 @@
+# Write Python code that initializes the variable
+# start_link to be the value of the position
+# where the first '<a href=' occurs in a page.
+
+page = '''<div id="top_bin"> <div id="top_content" class="width960">
+   <div class="udacity float-left"> <a href="/">'''
+
+start_link = page.find('<a href=')
+
 # Write Python code that assigns to the 
 # variable url a string that is the value 
 # of the first URL that appears in a link 

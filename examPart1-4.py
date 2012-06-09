10ec2d4350feac9c7f844a26ecfbcaaf757d430f
Alejandro Romero <alejo8591@gmail.com>
2012-06-09 00:29:02 -0500

    update algorithm for examParty1

--- examPart1-4.py
+++ examPart1-4.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
-def remove_tags(page):
-    data=[]
-    start_link = page.find('<')
-    start_link_close = page.find('>')
-    end_link = page.find('</')
-    end_link_close = page.find('>', start_link_close+1)
-    if(page[end_link_close+1]=='<'):
-    # eliminando espacios en blanco innecesarios
-      data = str(page[start_link_close+1:end_link].strip())
-      return data.split()
+def remove_tags(page, start_link=-1, start_link_close=-1, end_link=-1, end_link_close=-1, data = []):
+    if(start_link == -1):
+        start_link = page.find('<')
+        start_link_close = page.find('>')
+        end_link = page.find('</')
+        end_link_close = page.find('>', start_link_close+1)
+        if(start_link == -1):
+            return data
+        else:
+            page = page
+            data = page[start_link_close+1:end_link].strip().split()
+            return remove_tags(page, start_link, start_link_close, end_link, end_link_close, data)
+    elif(start_link < page.find('<', end_link_close)):
+        start_link = page.find('<', end_link_close)
+        start_link_close = page.find('>', start_link)
+        end_link = page.find('</', start_link)
+        end_link_close = page.find('>', end_link)
+        data = data + (page[start_link_close+1:end_link].strip().split())
+        start_link_diff = page.find('<', end_link_close)
+        if(start_link_diff == -1):
+            return data
+        else:
+            return remove_tags(page, start_link, start_link_close, end_link, end_link_close, data)
+    else:
+        return data

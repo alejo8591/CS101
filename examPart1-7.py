c8dd0626369a49e72b13a96c04b3eb62234b804e
Alejandro Romero <alejo8591@gmail.com>
2012-06-15 16:45:56 -0500

    update: ok exam Part1-7

--- examPart1-7.py
+++ examPart1-7.py
@@ -3,8 +3,7 @@
 #     to make a specific type of thing. It can 
 #     return anything you would find useful in apply_converter.
 def make_converter(match, replacement):
-    converter = {'m':match, 'r':replacement}
-    return converter
+    return {'m':match, 'r':replacement}
 #  apply_converter(converter, string)
 #     Takes as input a converter (produced by make_converter), and 
 #     a string, and returns the result of applying the converter to the 
@@ -12,8 +11,6 @@ def make_converter(match, replacement):
 #     build the converter, with the replacement.  It keeps doing 
 #     replacements until there are no more opportunities for replacements.
 def apply_converter(converter, string):
-    if(string.find(converter['m']) != -1):
+    while(string.find(converter['m']) != -1):
          string = string.replace(converter['m'],converter['r'])
-         return string
-    else:
-        return string
+    return string

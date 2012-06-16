d3cae34d578d8c37d287065318d33d789751c0a1
Alejandro Romero <alejo8591@gmail.com>
2012-06-16 18:25:01 -0500

    update ok algorithm exam part 1-9

--- examPart1-9.py
+++ examPart1-9.py
@@ -9,22 +9,29 @@
 
 # The procedure is_list below is from Homework 6. It returns True if 
 # p is a list and False if it is not.
-def is_list(q):
-    return isinstance(q, list)
+def is_list(p):
+    return isinstance(p, list)
+"""
+def deep_reverse(p):
+        try:
+            q
+        except NameError:
+            q = []
+        p.reverse()
+        i = 0
+        for i in p:
+            if(is_list(i) == True): deep_reverse(i)
+        q = p
+        return q
+"""
+def deep_reverse(p):
+    while is_list(p):
+        value = []
+        for i in range(len(p)-1, -1, -1):
+            value.append(deep_reverse(p[i]))
+        return value
+    return p
 
-def deep_reverse(list_reverse):
-            try:
-                q
-            except NameError:
-                q = []
-            
-            if q == []:q = q + list_reverse
-        
-            q.reverse()
-            i = 0
-            for i in q:
-                if(is_list(i) == True): deep_reverse(i)
-            return q
 #For example,
 
 p = [1, [2, 3, [4, [5, 6]]]]

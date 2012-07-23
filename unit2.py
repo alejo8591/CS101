4eeed86eedb401a602b31fb16eb413d18ddbcc33
Alejandro Romero <alejo8591@gmail.com>
2012-07-23 15:08:15 -0500

    update Unit 2

--- unit2.py
+++ unit2.py
@@ -161,7 +161,17 @@ def print_numbers(a):
     i=0
     while(i<a):
         i +=1
-        print i             
+        print i
+        
+# Other solution
+def print_numbers1(a):
+    i=1
+    while True:
+        if i > a: break
+        print i
+        i = i + 1
+    
+
 #print_numbers(3)
 #>>> 1
 #>>> 2
@@ -173,8 +183,7 @@ def print_numbers(a):
 # that number.
 def factorial(n):
     if(n==0):return 1
-    i=n
-    o=1
+    i, o= n, 1
     while(i!=0):
      o=o*i
      i-=1 
@@ -185,3 +194,120 @@ def factorial(n):
 #>>> 120
 #print factorial(6)
 #>>> 720
+# Modify the get_next_target procedure so that
+# if there is a link it behaves as before, but
+# if there is no link tag in the input string,
+# it returns None, 0.
+
+# Note that None is not a string and so should
+# not be enclosed in quotes.
+
+# Also note that your answer will appear in
+# parentheses if you print it.
+
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    #Insert your code below here
+    while start_link != -1: 
+         start_quote = page.find('"', start_link)
+         end_quote = page.find('"', start_quote + 1)
+         url = page[start_quote + 1:end_quote]
+         return url, end_quote
+    return None, 0
+
+url, endpos = get_next_target('hola mundo <a href="yopiando.com">Asi es</a>')
+if url: print "Yes"
+print "No"
+# Other Option
+def get_next_target1(page):
+    start_link = page.find('<a href=')
+    #Insert your code below here
+    if start_link != -1: return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+url, endpos = get_next_target('hola mundo <a href="yopiando.com">Asi es</a>')
+if url: print "Yes"
+print "No"
+
+# Print all links
+def print_all_links(page):
+    while True:
+        url, endpos = get_next_target(page)
+        if url:
+            print url
+            page = page[endpos:]
+        else:
+            break
+            # return "No"
+
+#-------------------- Udacify --------------------------------
+# Define a procedure, udacify, that takes as
+# input a string, and returns a string that
+# is an uppercase 'U' followed by the input string.
+# for example, when you enter
+
+# print udacify('dacians')
+
+# the output should be the string 'Udacians'
+
+# Make sure your procedure has a return statement.
+
+def udacify(a):
+    return 'U'+a
+
+# Remove the hash, #, from infront of print to test your code.
+
+#print udacify('dacians')
+#>>> Udacians
+
+#print udacify('turn')
+#>>> Uturn
+
+#print udacify('boat')
+#>>> Uboat
+
+# Define a procedure, median, that takes three
+# numbers as its inputs, and returns the median
+# of the three numbers.
+
+# Make sure your procedure has a return statement.
+
+def bigger(a,b):
+    if a > b: return a
+    return b
+
+def biggest(a,b,c):
+    return bigger(a,bigger(b,c))
+
+def median(a,b,c):
+    m = biggest(a,b,c)
+    if(a == m and a == b and a == c): return a
+    elif (a == m and a == b and a!= c): 
+        if a < c: return c
+        return a
+    elif (a == m and a == c and a != b):
+        if a < b: return b
+        return a
+    elif(b == m and b == c and b != a):
+        if b < a: return a
+        return b
+    elif (a == m and a != b and a != c):
+        if b < c: return c
+        return b
+    elif (b == m and b != a and b != c):
+        if a < c: return c
+        return a
+    elif (c == m and c !=a and c != b):
+        if a < b: return b
+        return a
+   
+#print(median(1,2,3))
+#>>> 2
+
+#print(median(9,3,6))
+#>>> 6
+
+#print(median(7,8,7))
+#>>> 7

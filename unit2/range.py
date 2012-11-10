241513bfba1114fbbef713dc3f19066c60b11ce0
Alejandro Romero <alejo8591@gmail.com>
2012-11-10 18:09:44 -0500

    update s problem unit2

--- unit2/range.py
+++ unit2/range.py
@@ -1,20 +1,24 @@
 def set_range(a,b,c):
-    if a > b:
-        if b > c:
+    if (a > b and a > c):
+        if (b > c and b < a):
             return a - c
         else:
             return a - b
-    elif b > a:
-        if a > c:
+    elif (b > a and b > c):
+        if (a > c and a < b):
             return b - c
         else:
             return b - a
-    elif c > a:
-        if a > b:
+    elif (c > a and c > b):
+        if (a > b and a < c):
             return c - b
         else:
             return c - a
-    elif a == b:
-        if a > c:
-            return a - c
-            
+    elif (a == b and a > c):
+        return a - c
+    elif (b == c and b > a):
+        return b - a
+    elif (a == c and a > b):
+        return a - b
+    elif (a == b and b == c):
+        return 0
